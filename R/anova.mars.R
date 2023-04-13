70594b90b8579b69d63682962e2037415ac3378f
EvanPu68 <yifanpu63@outlook.com>
2023-04-13 14:41:45 -0700

    14:41

--- R/anova.mars.R
+++ R/anova.mars.R
@@ -1,14 +1,28 @@
-#' anova implementation
+#' anova implementation - S3Method for an object of class 'mars'
 #'
-#' @param object a mars object
+#' @param object a mars object.
 #'
-#' @return anova information regarding the mars object
+#' @return anova information regarding the mars object and information of Basis
+#' functions.
 #' @export
 #'
 #' @examples
-#' mm<-mars(y~.,data=marstestdata)
+#' mm<-mars(ConcreteCompressiveStrength~.,data=concrete,mar.)
 #' anova(mm)
 anova.mars <- function(object) {
   fit <- lm(y~.-1,data=data.frame(y=object$y,object$B))
   anova(fit)
+  for (i in 1:length(names(object$B))){
+    cat(names(object$B)[[i]], ":","\n")
+    if(i == 1){
+      cat("Intercept", "\n")
+      next
+    }
+    Bf = object$Bfuncs[[i]]
+    for(j in 1:nrow(Bf))
+      cat("Component ",j,":", "\n",
+          "","Sign:",Bf[j,1],"\n",
+          "","Split Variable:",Bf[j,2],"\n",
+          "","Split Point:",Bf[j,3],"\n")
+  }
 }
